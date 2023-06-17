ec262d24773fcd9619bf1682744b32a68b8d6fc0
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-06-16 23:33:23 -0700

    update with views for calendar

--- client/src/scenes/calendar/index.jsx
+++ client/src/scenes/calendar/index.jsx
@@ -1,37 +1,38 @@
-import React, {useMemo} from "react";
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React, { useMemo } from "react";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Box } from "@mui/material";
 
 const localizer = momentLocalizer(moment);
 
-   const events = [
-     {
-       start: new Date(),
-       end: new Date(),
-       title: 'Event 1',
-     },
-     {
-       start: new Date(),
-       end: new Date(),
-       title: 'Event 2',
-     },
-   ];
+const events = [
+  {
+    start: new Date(),
+    end: new Date(),
+    title: "Event 1",
+  },
+  {
+    start: new Date(),
+    end: new Date(),
+    title: "Event 2",
+  },
+];
 
 const CalendarView = () => {
-
    
 
   return (
     <div>
-        <h1>Hello Calendar</h1>
-
-        <Box sx={{height:'100vh'}}>
-            <Calendar localizer={localizer} events={events} />
-        </Box>
+      <Box sx={{ height: "100vh" }}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          views={[Views.MONTH]}
+        />
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default CalendarView
+export default CalendarView;
