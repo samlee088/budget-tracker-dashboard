38b836f940775d112371b646ed8f504292adff01
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-07-22 16:35:35 -0700

    updates with troubleshooting login

--- client/src/scenes/login/index.jsx
+++ client/src/scenes/login/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, typographyClasses } from '@mui/material';
 import { useAddUserMutation, useLoginUserMutation } from 'state/api';
 import Auth from '../../utils/auth';
 
@@ -11,6 +11,9 @@ const Login = () => {
   const emailRef = useRef(null);
   const passRef = useRef(null);
   const userNameRef = useRef(null);
+
+  const [loginResponse, setLoginResponse] = useState('')
+  const [loginError, setLoginError] = useState('');
   
   const [addUserPost, { isLoading: isLoadingAddUser }] = useAddUserMutation();
   const [loginUserPost, { isLoading: isLoadingLoginUser }] = useLoginUserMutation();
@@ -32,6 +35,7 @@ const Login = () => {
       );
 
       console.log(response);
+      setLoginResponse(response.data.message);
 
       const { token, user } = response.data;
 
@@ -52,25 +56,31 @@ const Login = () => {
     e.preventDefault();
 
     try {
-
       console.log(loginPasswordRef.current.value);
       console.log(loginUserNameRef.current.value);
-
-
+  
       const response = await loginUserPost({
-          password: loginPasswordRef.current.value,
-          username: loginUserNameRef.current.value,
+        password: loginPasswordRef.current.value,
+        username: loginUserNameRef.current.value,
       });
-
-     console.log(response)
-
-      const {user, token } = response.data;
-
-      console.log( token );
-      console.log( user );
-
-      Auth.login(token);
-
+  
+      console.log(response);
+  
+      if (response.error) {
+        // If there is an error, set the state with error data
+        setLoginResponse(response.error.data.status);
+        setLoginError(response.error.data.message);
+      } else {
+        // If there is no error, proceed with login logic
+        const { user, token } = response.data;
+  
+        console.log(token);
+        console.log(user);
+  
+        // Move Auth.login(token) before state-setting functions
+        Auth.login(token);
+      }
+  
       // Handle the response here
     } catch (error) {
       console.error(error);
@@ -83,57 +93,79 @@ const Login = () => {
 
   return (
     <div>
+
       <h1>Login Page</h1>
-    <Box sx={{ 
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      flexDirection: 'column'
-    }}>
 
-    <button type = 'submit' onClick = { login }>
-      Login
-    </button>
+      <Box sx={{ 
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
 
-      <Box sx={{m: '20px'}}>
-        <form>
-          <input
-            ref = { loginUserNameRef }
-            type = 'text'
-            placeholder = 'User Name'
-          />
-          <input 
-            ref = { loginPasswordRef }
-            type = 'password'
-            placeholder = 'Password'
-          />
-        </form>
-      </Box>
+        <button type = 'submit' onClick = { login }>
+          Login
+        </button>
 
-    <button type = 'submit' onClick = { register }>
-      Sign Up
-    </button>
-
-      <Box sx={{m: '20px'}}>
-        <form>
+        <Box sx={{m: '20px'}}>
+          <form>
             <input
-              ref = { emailRef }
-              type = 'email'
-              placeholder = 'Email Address'
-            />
-            <input 
-              ref = { passRef }
-              type = 'password'
-              placeholder = 'Password'
-            />
-              <input 
-              ref = { userNameRef }
+              ref = { loginUserNameRef }
               type = 'text'
               placeholder = 'User Name'
             />
+            <input 
+              ref = { loginPasswordRef }
+              type = 'password'
+              placeholder = 'Password'
+            />
+          </form>
+        </Box>
+
+        <Box sx={{ m: '20px' }}>
+          {loginResponse && (
+            <>
+              <Typography>Login Response</Typography>
+              <br />
+              <Typography>{loginResponse}</Typography>
+            </>
+          )}
+        </Box>
+
+        <Box sx={{ m: '20px' }}>
+            {loginError && (
+              <>
+                <Typography>Login Error</Typography>
+                <br/>
+                <Typography>{loginError}</Typography>
+              </>
+            )}
+        </Box>
+
+        <button type = 'submit' onClick = { register }>
+          Sign Up
+        </button>
+
+        <Box sx={{m: '20px'}}>
+          <form>
+              <input
+                ref = { emailRef }
+                type = 'email'
+                placeholder = 'Email Address'
+              />
+              <input 
+                ref = { passRef }
+                type = 'password'
+                placeholder = 'Password'
+              />
+                <input 
+                ref = { userNameRef }
+                type = 'text'
+                placeholder = 'User Name'
+              />
             </form>
         </Box>
-    </Box>
+      </Box>
 
     </div>
   )
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -16,13 +16,13 @@ export const login = async ({ body }, res) => {
   const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
 
   if (!user) {
-    return res.status(400).json({ message: "Can't find this user" });
+    return res.status(400).json({ status: "Unsuccessful Login", message: "Can't find this user" });
   }
 
   const correctPw = await user.isCorrectPassword(body.password);
 
   if (!correctPw) {
-    return res.status(400).json({ message: 'Wrong password!' });
+    return res.status(400).json({ status: "Unsuccessful Login", message: 'Wrong password!' });
   }
   const token = signToken(user);
   res.json({ token, user });
